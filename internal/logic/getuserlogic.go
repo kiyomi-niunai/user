c5df7ff2d44d505eedf24e3b499965a2b5e44b94
caiqiqin <qiqincai@126.com>
2021-11-07 15:52:05 +0800

    adf

--- internal/logic/getuserlogic.go
+++ internal/logic/getuserlogic.go
@@ -2,10 +2,10 @@ package logic
 
 import (
 	"context"
-	"fmt"
-	"github.com/kiyomi-niunai/user/model"
-	"github.com/tal-tech/go-zero/core/stores/cache"
-	"github.com/tal-tech/go-zero/core/stores/sqlx"
+	//"fmt"
+	//"github.com/kiyomi-niunai/user/model"
+	//"github.com/tal-tech/go-zero/core/stores/cache"
+	//"github.com/tal-tech/go-zero/core/stores/sqlx"
 
 	"github.com/kiyomi-niunai/user/internal/svc"
 	"github.com/kiyomi-niunai/user/user"
@@ -28,14 +28,14 @@ func NewGetUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserLo
 
 func (l *GetUserLogic) GetUser(in *user.IdRequest) (*user.UserResponse, error) {
 	// todo: add your logic here and delete this line]
-	var conn sqlx.SqlConn
-	var c cache.CacheConf
-	userObj, err := model.NewUsersModel(conn, c).FindOne(10000619)
-	if err != nil {
-		fmt.Println("报错的是", err)
-	}
+	//var conn sqlx.SqlConn
+	//var c cache.CacheConf
+	//userObj, err := model.NewUsersModel(conn, c).FindOne(10000619)
+	//if err != nil {
+	//	fmt.Println("报错的是", err)
+	//}
 	return &user.UserResponse{
-		Id: string(userObj.Id),
-		Name: userObj.Name,
+		Id: "222",
+		Name: "test",
 	}, nil
 }
