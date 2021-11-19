69dd4daa05862d6ad75ebff4bf6b885eda8592eb
caiqiqin <qiqincai@126.com>
2021-11-19 17:08:03 +0800

    相关代码fix

--- user.go
+++ user.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"github.com/tal-tech/go-zero/core/logx"
 
 	"github.com/kiyomi-niunai/user/internal/config"
 	"github.com/kiyomi-niunai/user/internal/server"
@@ -12,6 +14,7 @@ import (
 	"github.com/tal-tech/go-zero/core/conf"
 	"github.com/tal-tech/go-zero/core/service"
 	"github.com/tal-tech/go-zero/zrpc"
+	"golang.org/x/time/rate"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -38,7 +41,19 @@ func main() {
 
 	})
 	defer s.Stop()
+	s.AddUnaryInterceptors(limitInterceptor)
 
 	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
 	s.Start()
+}
+
+//拦截验证器
+func limitInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	var limiter = rate.NewLimiter(rate.Limit(100), 10)
+	if !limiter.Allow() {
+		fmt.Println("限流了")
+		return nil, nil
+	}
+	logx.Info("好玩")
+	return handler(ctx, req)
 }
